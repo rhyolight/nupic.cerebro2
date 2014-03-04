3dc174013c58532fb119175e74ea059577d9cc17
Chetan Surpur <chetan51@gmail.com>
2014-03-03 23:32:47 -0800

    Added ability to change particle system vertices on the fly

--- static/js/classes/three_d_visualization.js
+++ static/js/classes/three_d_visualization.js
@@ -4,6 +4,8 @@ var ThreeDVisualization = Visualization.extend(function(base) {
 			base.init(container);
 
 			this.particleSystem = null;
+
+			this._initParticleSystem();
 		},
 
 		/* Public */
@@ -14,16 +16,10 @@ var ThreeDVisualization = Visualization.extend(function(base) {
 			    originY = -(numY * padding) / 2,
 			    originZ = -(numZ * padding) / 2;
 
-			var particles = new THREE.Geometry(),
-			    material = new THREE.ParticleBasicMaterial({
-			      color: 0xFFFFFF,
-			      size: 20,
-			      map: THREE.ImageUtils.loadTexture(
-				    "img/particle.png"
-				  ),
-				  blending: THREE.AdditiveBlending,
-				  transparent: true
-			    });
+			var particleSystem = this.particleSystem,
+			    particles = particleSystem.geometry;
+
+			particles.vertices = [];
 
 			for (var x = 0; x < numX; x++) {
 				for (var y = 0; y < numY; y++) {
@@ -38,10 +34,28 @@ var ThreeDVisualization = Visualization.extend(function(base) {
 				}
 			}
 
+			particles.__dirtyVertices = true;
+		},
+
+		/* Public */
+		_initParticleSystem: function() {
+			var particles = new THREE.Geometry(),
+			    material = new THREE.ParticleBasicMaterial({
+			      color: 0xFFFFFF,
+			      size: 20,
+			      map: THREE.ImageUtils.loadTexture(
+				    "img/particle.png"
+				  ),
+				  blending: THREE.AdditiveBlending,
+				  transparent: true
+			    });
+
 			var particleSystem = new THREE.ParticleSystem(particles, material);
 		    particleSystem.sortParticles = true;
 
 			this.scene.add(particleSystem);
+
+			this.particleSystem = particleSystem;
 		}
 	};
 });
