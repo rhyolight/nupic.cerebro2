db699f8df03d641d0fc7c21237296a5a32169215
Chetan Surpur <chetan51@gmail.com>
2014-03-05 17:41:18 -0800

    Refactored code

--- static/js/main.js
+++ static/js/main.js
@@ -5,13 +5,23 @@ var history = new History();
 var visualization = new ThreeDVisualization(container, history);
 visualization.render();
 
-// Test dynamically adding snapshots to history
+// Test dynamically adding random snapshots to history
 setInterval(function() {
-    var x = Math.ceil(Math.random() * 50),
-        y = Math.ceil(Math.random() * 10),
-        z = Math.ceil(Math.random() * 47) + 3;
-
-    var snapshot = new TestNetworkSnapshot([x, y, z], [1, 2, 3], [4, 5, 6]);
+    var snapshot = generateRandomSnapshot(1, 100, 1, 50, 3, 25);
     history.addSnapshot(snapshot);
     visualization.historyUpdated();
 }, 1000);
+
+/* Utilities */
+
+function generateRandomSnapshot(minX, maxX, minY, maxY, minZ, maxZ) {
+    var x = generateRandomValue(minX, maxX),
+        y = generateRandomValue(minY, maxY),
+        z = generateRandomValue(minZ, maxZ);
+
+    return new TestNetworkSnapshot([x, y, z], [1, 2, 3], [4, 5, 6]);
+}
+
+function generateRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
