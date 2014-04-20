5faa9b8ca59d7b93312aee8bd8bb040d3daa6aab
Chetan Surpur <chetan51@gmail.com>
2014-04-19 17:03:14 -0700

    Add proximal synapses

--- static/js/classes/network_layer.js
+++ static/js/classes/network_layer.js
@@ -29,6 +29,23 @@ var NetworkLayer = AbstractLayer.extend(function(base) {
 
         getPredictedCells: function(callback) {
             this.getList("predicted_cells", callback);
+        },
+
+        getProximalSynapses: function(callback) {
+            var self = this,
+                dimensions = this.dimensions,
+                cellsPerColumn = dimensions[2];
+
+            this.getList("proximal_synapses", function(error, proximalSynapses) {
+                for (var i = 0; i < proximalSynapses.length; i++) {
+                    // Convert column index to index of first cell in the column
+                    var synpase = proximalSynapses[i];
+                    synpase[0] *= cellsPerColumn;
+                    proximalSynapses[i] = synpase;
+                }
+
+                callback(error, proximalSynapses);
+            });
         }
     };
 });
