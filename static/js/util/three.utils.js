c019a23c04b6ecb67ec10e8b78a058b1d12c95ac
Chetan Surpur <chetan51@gmail.com>
2014-05-05 22:21:48 -0700

    Improved createLabel

--- static/js/util/three.utils.js
+++ static/js/util/three.utils.js
@@ -1,7 +1,7 @@
 var Utils = {};
 
-/* Taken from https://gist.github.com/ekeneijeoma/1186920 */
-Utils.createLabel = function(text, x, y, z, size, color, backGroundColor, backgroundMargin) {
+/* From https://gist.github.com/ekeneijeoma/1186920 */
+Utils.createLabel = function(text, x, y, z, size, color, backgroundColor, backgroundMargin) {
     if(!backgroundMargin)
         backgroundMargin = 50;
 
@@ -17,8 +17,8 @@ Utils.createLabel = function(text, x, y, z, size, color, backGroundColor, backgr
     context = canvas.getContext("2d");
     context.font = size + "pt Arial";
 
-    if(backGroundColor) {
-        context.fillStyle = backGroundColor;
+    if(backgroundColor) {
+        context.fillStyle = backgroundColor;
         context.fillRect(canvas.width / 2 - textWidth / 2 - backgroundMargin / 2, canvas.height / 2 - size / 2 - +backgroundMargin / 2, textWidth + backgroundMargin, size + backgroundMargin);
     }
 
@@ -26,9 +26,6 @@ Utils.createLabel = function(text, x, y, z, size, color, backGroundColor, backgr
     context.textBaseline = "middle";
     context.fillStyle = color;
     context.fillText(text, canvas.width / 2, canvas.height / 2);
-
-    // context.strokeStyle = "black";
-    // context.strokeRect(0, 0, canvas.width, canvas.height);
 
     var texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
@@ -38,10 +35,9 @@ Utils.createLabel = function(text, x, y, z, size, color, backGroundColor, backgr
     });
 
     var mesh = new THREE.Mesh(new THREE.PlaneGeometry(canvas.width, canvas.height), material);
-    // mesh.overdraw = true;
     mesh.doubleSided = true;
-    mesh.position.x = x - canvas.width;
-    mesh.position.y = y - canvas.height;
+    mesh.position.x = x;
+    mesh.position.y = y;
     mesh.position.z = z;
 
     return mesh;
