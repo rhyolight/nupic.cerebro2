7747023092cfd0734ffc8f9fe1eacac263da23c0
Chetan Surpur <chetan51@gmail.com>
2014-05-14 22:01:27 -0700

    Remove test files

--- static/js/classes/abstract_model.js
+++ static/js/classes/abstract_model.js
@@ -1,18 +1,6 @@
 var AbstractModel = Fiber.extend(function() {
     return {
-        init: function() {
-            this.inputLayers = [];
-        },
-
-        /* Public */
-
-        pushInputLayer: function(inputLayer) {
-            this.inputLayers.push(inputLayer);
-        },
-
-        popInputLayer: function() {
-            return this.inputLayers.pop();
-        },
+        init: function() {},
 
         /* To override
             Note: callbacks params follow Node.js convention of (error, retVal)
