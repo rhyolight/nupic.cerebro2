40ce8445f55cfa1d4d6a29d9d5f9b47074a7f3e8
Chetan Surpur <chetan51@gmail.com>
2014-05-16 16:38:37 -0700

    Added camera controls

--- static/js/classes/coordinate_encoder_visualization.js
+++ static/js/classes/coordinate_encoder_visualization.js
@@ -30,6 +30,18 @@ var CoordinateEncoderVisualization = EncoderVisualization.extend(function(base) 
             });
         },
 
+        initGUI: function() {
+            base.initGUI.call(this);
+
+            this.resetCamera = this._resetCamera;
+            this.focusCamera = this._focusCamera;
+
+            var cameraFolder = this.gui.addFolder('Camera');
+            cameraFolder.add(this, 'resetCamera');
+            cameraFolder.add(this, 'focusCamera');
+            cameraFolder.open();
+        },
+
         /* Private */
 
         _redraw: function() {
@@ -41,6 +53,34 @@ var CoordinateEncoderVisualization = EncoderVisualization.extend(function(base) 
             coordinateDrawing.updateParticles();
 
             scene.add(coordinateDrawing.getObject3D());
+        },
+
+        _resetCamera: function() {
+            var camera = this.camera,
+                controls = this.controls;
+
+            camera.position.set(300, 500, 1200);
+            controls.target = new THREE.Vector3(0, 0, 0);
+        },
+
+        _focusCamera: function() {
+            var camera = this.camera,
+                controls = this.controls,
+                coordinateDrawing = this.coordinateDrawing,
+                particleSystem = coordinateDrawing.particleSystem;
+                geometry = particleSystem.geometry;
+
+            geometry.computeBoundingBox();
+
+            var min = geometry.boundingBox.min,
+                max = geometry.boundingBox.max,
+                size = new THREE.Vector3().subVectors(max, min),
+                center = new THREE.Vector3().addVectors(min, max).divideScalar(2);
+                z = Math.max.apply(Math, size.toArray()) * 5,
+                overCenter = new THREE.Vector3().copy(center).setZ(z);
+
+            camera.position = overCenter;
+            controls.target = center;
         },
     };
 });
